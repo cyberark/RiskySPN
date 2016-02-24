9950ef2fc4a4a1182d462462a5f4f79dbeaf23e8
CyberArkLabs <CyberArk.Labs@cyberark.com>
2016-02-24 15:03:01 +0200

    Update RiskySPNs.psm1

--- RiskySPNs.psm1
+++ RiskySPNs.psm1
@@ -1,2 +1,3 @@
-﻿# PowerShell Module file
+﻿# Import this module to use all RiskySPNs tools
+﻿# Import-Module .\RiskySPNs.psm1
 Get-ChildItem $PSScriptRoot | ? { $_.PSIsContainer } | % { Import-Module $_.FullName -DisableNameChecking}
