b4050d2acf9764da0aa2a57b1af09b92ebd3d640
CyberArkLabs <CyberArk.Labs@cyberark.com>
2016-02-24 15:59:37 +0200

    Update RiskySPNs.psm1

--- RiskySPNs.psm1
+++ RiskySPNs.psm1
@@ -1,3 +1,10 @@
-﻿# Import this module to use all RiskySPNs tools
-﻿# Import-Module .\RiskySPNs.psm1
-Get-ChildItem (Join-Path $PSScriptRoot *.ps1) | % { . $_.FullName -DisableNameChecking}
+<# 
+    Author: Matan Hart
+    Contact: cyberark.labs@cyberark.com
+    License: GNU v3
+    Requirements: PowerShell v3+
+
+Import this module to use all RiskySPNs tools
+Import-Module .\RiskySPNs.psm1
+#>
+Get-ChildItem (Join-Path $PSScriptRoot *.ps1) | % {Import-Module $_.FullName -WarningAction SilentlyContinue}
