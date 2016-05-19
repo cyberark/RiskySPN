d0c8efefb35c7e23af4c26d94f983edd39ee2b9c
Matan Hart <me@matanhart.com>
2016-05-19 14:37:44 +0300

    Support PowerShell v2 now

--- RiskySPNs.psm1
+++ RiskySPNs.psm1
@@ -1,10 +1,11 @@
 <# 
-    Author: Matan Hart
+    Author: Matan Hart (@machosec)
     Contact: cyberark.labs@cyberark.com
     License: GNU v3
-    Requirements: PowerShell v3+
-
-Import this module to use all RiskySPNs tools
-Import-Module .\RiskySPNs.psm1
+    Required Dependencies: None
+    Optional Dependencies: None
 #>
+#Import this module to use all RiskySPNs scripts (require all .ps1 files to be in the directory)
+
+#PS C:\> Import-Module .\RiskySPNs.psm1
 Get-ChildItem (Join-Path $PSScriptRoot *.ps1) | % {Import-Module $_.FullName -WarningAction SilentlyContinue}
